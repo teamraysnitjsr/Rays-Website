cad2119eb62e1c529e1f329dcc0856041313f641
Yash <ybaranwal5@gmail.com>
2021-08-05 19:52:49 +0530

    basic about page

--- src/Pages/About/about.pages.jsx
+++ src/Pages/About/about.pages.jsx
@@ -1,5 +1,44 @@
 import React from "react";
+import "./about.pages.css"
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
 const About = () => {
-  return <h1>ABOUT PAGE</h1>;
+  return (
+    <div>
+    <div className="heading">
+
+    <h1 >ABOUT US</h1>
+    </div>
+     <p className="info">The constant depletion of non-renewable energy resources is one of the major threat these days and this threat compelled the students of 2K12 batch of N.I.T Jamshedpur to look over some alternative energy resources and with this idea TEAM RAYS came into existence. It is the first ever initiative by the students of N.I.T Jamshedpur to develop a solar electric vehicle. The team aims at developing a highly efficient solar electric vehicle to provide a breakthrough in the application of solar technology to the transportation sectors which is compatible for Indian roads as well. The outlook of our team is to develop an eco-friendly, economic and efficient solar car that could meet the current energy demands. The team came into existence in the year 2014 with its dedicated founder Amber Katyayan and co-founder Sanket Raj and is currently under the guidance of third year students .</p>
+     <div className="works">
+
+     <h1 >OUR WORKS</h1>
+     <p>The team aims at developing a highly efficient solar electric vehicle to provide a breakthrough in the application of solar technology 
+     to the transportation sector.We aim to develop easily available & low cost technology, gain practical experience in design and fabrication 
+     and develop technical skills in students as well as management and leadership quality. Our main motto is to develop an eco-friendly, economic 
+     and efficient solar car. We are trying to develop an innovative solar car which is able to run as good as petrol or diesel engine cars.</p>
+     
+     <p>filter pending</p>
+     
+     </div>
+     <div className="process">
+
+     <h1 >OUR PROCESS</h1>
+     <p>Our prime goal is the development of an energy efficient passenger SEV suitable for Indian roads.Fuelled by its member's passion for environment friendly and sustainable technologies, the team has to go through many stages of work and it takes about an year to build a car.....</p>
+      <div className="icons">
+      <i className=" i1 fas fa-mug-hot"></i>
+     <i class="i1 fas fa-bullhorn"></i>
+     <i className="i1 fas fa-image"></i>
+     <i className="i1 far fa-heart"></i>
+     <i className="i1 fas fa-shopping-cart"></i>
+     <i className="i1 fas fa-rocket"></i>
+
+      </div>
+     
+     </div>
+     </div>
+
+  );
+  
 };
 export default About;
